f8459392679c2a94f7d176a08b69b864a8f24d07
cnbluegeek <cnbluegeek@gmail.com>
2020-01-07 17:55:32 +0800

    feat(run.sh):add batch_size setting

--- run.sh
+++ run.sh
@@ -15,10 +15,17 @@ if [ $# -ge 2 ]; then
 fi
 echo "set background=${bg}"
 
+# set batch size
+bs=64
+if [ $# -ge 3 ]; then
+    bs=$3
+fi
+echo "set batch_size=${bs}"
+#exit
 # modify this variable to start other process
 process_name="keras_exp.train_vgg_on_celeba"
 if [ ${bg} = "yes" ]; then
-    python3 -m ${process_name} --phase=${phase} >out.${phase}.log 2>&1 &
+    python3 -m ${process_name} --phase=${phase} --batch-size=${bs} >out.${phase}.log 2>&1 &
 else
-    python3 -m ${process_name} --phase=${phase}
+    python3 -m ${process_name} --phase=${phase} --batch-size=${bs} 
 fi
